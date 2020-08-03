845a97b526a9d4fa989c652b9ada9df9dd2a9b8a
Aidanas93 <aidanas93@gmail.com>
2020-08-03 18:56:09 +0300

    Finished initial kata task

---
+++ Kata.BowlingGame.Tests/BowlingScoreCalculatorTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace Kata.BowlingGame.Tests
+{
+    public class BowlingScoreCalculatorTests
+    {
+        [Theory]
+        [InlineData("12345123451234512345", 60)]
+        [InlineData("9-9-9-9-9-9-9-9-9-9-", 90)]
+        [InlineData("5/5/5/5/5/5/5/5/5/5/5", 150)]
+        [InlineData("xxxxxxxxxxxx", 300)]
+        [InlineData("xxxxxxxxxx52", 282)]
+        [InlineData("52xx3/52x112/x53", 132)]
+        [InlineData("2/x543/215/5/80x5/3", 137)]
+        [InlineData("61200/4/3/8/x1443xx2", 123)]
+        public void CalculateScore_ValidInput_CalculatesScoreCorrectly(string input, ushort expectedScore)
+        {
+            ushort actualScore = BowlingScoreCalculator.CalculateScore(input);
+
+            Assert.Equal(expectedScore, actualScore);
+        }
+    }
+}
---
+++ Kata.BowlingGame/BowlingScoreCalculator.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.BowlingGame
+{
+    public enum StrikeType
+    {
+        Number,
+        Spare,
+        Strike
+    }
+    public static class BowlingScoreCalculator
+    {
+        
+        public static ushort CalculateScore(string gamescore)
+        {
+            gamescore = gamescore.ToLower().Replace('-', '0');
+
+            Node firstNode = null;
+            Node prevNode = null;
+            foreach (var ch in gamescore)
+            {
+
+                ushort numberValue;
+                StrikeType type;
+                if (UInt16.TryParse(ch.ToString(), out numberValue))
+                {
+                    type = StrikeType.Number;
+                }
+                else if (ch == '/')
+                {
+                    type = StrikeType.Spare;
+                    numberValue = (ushort)(10 - prevNode.Value);
+                }
+                else
+                {
+                    type = StrikeType.Strike;
+                    numberValue = 10;
+                }
+
+                Node node = new Node()
+                {
+                    Value = numberValue,
+                    Type = type,
+                    PrevNode = prevNode,
+                };
+
+                if (prevNode != null)
+                {
+                    prevNode.NextNode = node;
+                }
+                else
+                {
+                    firstNode = node;
+                }
+
+                prevNode = node;
+            }
+
+            ushort score = 0;
+            ushort frameCount = 0;
+            Node currentNode = firstNode;
+            while (true)
+            {
+                frameCount++;
+                if (currentNode.Type == StrikeType.Number)
+                {
+                    if (currentNode.NextNode.Type == StrikeType.Spare)
+                    {
+                        score += (ushort)(currentNode.Value + currentNode.NextNode.Value + currentNode.NextNode.NextNode.Value);
+
+                        if (currentNode.NextNode.NextNode.NextNode == null)
+                            break;
+                    }
+                    else
+                    {
+                        score += (ushort)(currentNode.Value + currentNode.NextNode.Value);
+
+                        if (currentNode.NextNode.NextNode == null)
+                            break;
+                    }
+                    currentNode = currentNode.NextNode.NextNode;
+                }
+                else
+                {
+                    score += (ushort)(currentNode.Value + currentNode.NextNode.Value + currentNode.NextNode.NextNode.Value);
+
+                    if (currentNode.NextNode.NextNode.NextNode == null && frameCount != 9)
+                        break;
+
+                    currentNode = currentNode.NextNode;
+                }
+            }
+
+            return score;
+        }
+    }
+}
